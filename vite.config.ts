8b7ed4171d215c52f82e62b37f5bc2fbf6e0e339
hariharan4814 <hariharan4814@gmail.com>
2025-10-31 13:57:25 +0530

    Render issue solved

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-<<<<<<< HEAD
-=======
-import { componentTagger } from "lovable-tagger";
->>>>>>> ff1267d232b76d32b947929cb3d37a609241be3b
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // ✅ correct for custom domain (e.g., hariharan.me)
+  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
-<<<<<<< HEAD
   plugins: [react()].filter(Boolean),
-=======
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
->>>>>>> ff1267d232b76d32b947929cb3d37a609241be3b
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
